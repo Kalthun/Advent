555c96a298435a5d975de51fc0d06741dc1f22db
James McDonagh <jwmcdona@lakeheadu.ca>
2023-12-17 01:49:56 -0500

    saving ideas for tomorrow

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+struct Point
+{
+    r_index: i32,
+    c_index: i32,
+}
+
 fn main() -> std::io::Result<()> {
 
     // Open the File for Reading
@@ -8,7 +14,15 @@ fn main() -> std::io::Result<()> {
 
     let reader = BufReader::new(file);
 
+    // need to track prev and next line
 
+    // need to have a checker that when you hit a symbol (!char.is_numeric and != '.') then you check around it
+
+    // create a Vector to store all numbers found and what indexes they are? (as tuples)
+
+    // we can remove them if their position is too close
+
+    // maybe create a Point class for fun
 
 
     Ok(())
