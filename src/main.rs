dd103c95df1d15aad7661704c6a29aad397e1707
James McDonagh <jwmcdona@lakeheadu.ca>
2023-12-16 21:03:47 -0500

    Part 2 done

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::io::{BufRead, BufReader};
 fn main() -> std::io::Result<()> {
 
 
-    let mut total: Vec<u32> = Vec::new();
+    let mut total: Vec<i32> = Vec::new();
 
     // Open the File for Reading
-    let file = File::open("input.txt")?;
+    let file = File::open("Q1_input.txt")?;
 
     let reader = BufReader::new(file);
 
@@ -16,7 +16,7 @@ fn main() -> std::io::Result<()> {
         total.push(get_line_number(line.unwrap()));
     }
 
-    let answer:u32 = total.iter().sum();
+    let answer:i32 = total.iter().sum();
 
     println!("answer is: {}", answer);
 
@@ -24,27 +24,56 @@ fn main() -> std::io::Result<()> {
 
 }
 
-fn get_line_number(line:String) -> u32
+fn transform_string(string:&String) -> String
+{
+    let valid = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut temp = 0;
+
+    for (index, num) in valid.iter().enumerate()
+    {
+        if string.contains(num)
+        {
+            temp = index + 1;
+            break;
+        }
+    }
+
+    return temp.to_string();
+}
+
+fn get_line_number(line:String) -> i32
 {
 
-    let mut start = ' ';
-    let mut end = ' ';
+    let mut temp = String::from("");
+    let mut start = String::from(" ");
+    let mut end = String::from(" ");
 
     for c in line.chars()
     {
         if c.is_numeric()
         {
-            if start == ' '
+            if start == " "
             {
-                start = c;
-                end = c;
+                start = c.to_string();
             }
-            else
+            end = c.to_string();
+            temp = "".to_string();
+        }
+        else
+        {
+            temp = temp + c.to_string().as_str();
+            let ret = transform_string(&temp);
+            if ret != "0"
             {
-                end = c;
+                if start == " "
+                {
+                    start = ret.to_string();
+                }
+                end = ret.to_string();
+                temp = temp.chars().last().unwrap().to_string();
             }
         }
     }
 
-    return 10 * (start.to_digit(10).unwrap()) + (end.to_digit(10).unwrap());
+    return 10 * (start.parse::<i32>().unwrap()) + (end.parse::<i32>().unwrap());
 }
