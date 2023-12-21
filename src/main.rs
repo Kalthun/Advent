395503a59eac821e52834e5ac7e6f34f3acbd707
James McDonagh <jwmcdona@lakeheadu.ca>
2023-12-20 21:59:19 -0500

    tab is driving me crazy..

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,21 @@ use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()>
 {
-    let file = File::open("Q7_input.txt")?;
-    let reader = BufReader::new(file);
+    let file = File::open("Q8_input.txt")?;
+    let mut reader = BufReader::new(file);
 
+    let mut buffer = String::new();
+    let _ = reader.read_line(&mut buffer);
 
+    let directions:Vec<char> = buffer.chars().filter(|c| *c == 'R' || *c == 'L').collect();
+
+    let _ = reader.read_line(&mut buffer);
+
+    println!("directions:{:?}", directions);
+
+    for line in reader.lines()
+    {
+    }
 
     Ok(())
 }
